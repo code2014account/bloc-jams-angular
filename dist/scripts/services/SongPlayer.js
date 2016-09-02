7220632ef869301840f844e97204016bd42a2207
Matt <matt24ray@gmail.com>
2016-09-02 12:16:38 -0500

    play button not working

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,6 +1,16 @@
 (function() {
   function SongPlayer() {
     var SongPlayer = {};
+
+    SongPlayer.play = function(song) {
+      var currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+      });
+
+      currentBuzzObject.play();
+    };
+
     return SongPlayer;
   }
 
